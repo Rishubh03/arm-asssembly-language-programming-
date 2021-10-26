f3bde24dd8cba6b0baa4f969f607236f3374ae9f
Rishubh Kumar <82128737+Rishubh03@users.noreply.github.com>
2021-10-26 19:38:42 +0530

    Create CountZeros.s

---
+++ CountZeros.s
@@ -0,0 +1,45 @@
+//Write an Assembly Language Program to Find No of 0’s present in a 32 bit number.
+
+VALUE1 EQU 0X1FFFF000
+VALUE2 EQU 0X1FFFF01B
+		AREA TEST,CODE,READONLY
+		EXPORT __main
+__main
+		
+		LDR R0,=VALUE1
+		LDR R1,=VALUE2
+		MOVS R2,#0
+		MOVS R5,#0
+		
+		
+L1	    MOVS R6,#0
+		LDRB R3,[R0]
+L2		MOVS R4,#1
+		TST R4,R3
+		BEQ L3
+		ASRS R3,R3,#1
+		ADDS R6,#1
+		CMP R6,#4
+		BLT L2	
+		ADDS R0,#1
+		ADDS R5,#1
+		CMP R5,#4
+		BLT L1
+		B L4
+		
+L3		ADDS R2,#1
+		ASRS R3,R3,#1
+		ADDS R6,#1
+		CMP R6,#4
+		BLT L2		
+		ADDS R0,#1
+		ADDS R5,#1
+		CMP R5,#4
+		BLT L1
+		B L4
+	
+L4   	STRB R2,[R1]
+		NOP
+		END		
+
+	
